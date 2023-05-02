9574025b32f34f44946a2abaf5712ca961d5d582
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-02 11:34:44 -0700

    using redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,8 @@
 import { ScrollView, Text } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+
 
 const Mission = () => {
     return (
@@ -16,17 +17,21 @@ const Mission = () => {
 }
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS)
+    const partners = useSelector((state) => state.partners);
+
     return (
         <ScrollView>
             <Mission />
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.map((partner) => {
+                {partners.partnersArray.map((partner) => {
                     return (
                         <ListItem key={partner.id}>
-                            <Avatar rounded source={partner.image} />
+                            <Avatar 
+                                rounded 
+                                source={{ uri: baseUrl + partner.image}} 
+                            />
                             <ListItem.Content>
                                 <ListItem.Title>
                                     {partner.name}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { Card } from "react-native-elements";
-import { CAMPSITES } from "../shared/campsites";
-import { PROMOTIONS } from "../shared/promotions";
-import { PARTNERS } from "../shared/partners";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
 
 const FeaturedItem = ({ item }) => {
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
-                <Card.Image source={item.image}>
+                <Card.Image source={{ uri: baseUrl + item.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
@@ -22,7 +20,7 @@ const FeaturedItem = ({ item }) => {
                         </Text>
                     </View>
                 </Card.Image>
-                <Text style={{margin: 20 }}>{item.description}</Text>
+                <Text style={{ margin: 20 }}>{item.description}</Text>
             </Card>
         )
     }
@@ -31,15 +29,15 @@ const FeaturedItem = ({ item }) => {
 
 
 const HomeScreen = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [promotions, setPromotions] = useState(PROMOTIONS);
-    const [partners, setPartners] = useState(PARTNERS);
+    const campsites = useSelector((state) => state.campsites);
+    const promotions = useSelector((state) => state.promotions);
+    const partners = useSelector((state) => state.partners);
 
-    const featCampsite = campsites.find((item) => item.featured);
-    const featPromotion = promotions.find((item) => item.featured);
-    const featPartner = partners.find((item) => item.featured);
+    const featCampsite = campsites.campsitesArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find((item) => item.featured);
+    const featPartner = partners.partnersArray.find((item) => item.featured);
 
-    return(
+    return (
         <ScrollView>
             <FeaturedItem item={featCampsite} />
             <FeaturedItem item={featPromotion} />
