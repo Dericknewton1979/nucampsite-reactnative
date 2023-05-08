d7cb386b70451a970318319e0ec0bdd72d7413eb
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-08 12:35:52 -0700

    swipe and delete favs

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,6 +1,14 @@
-import { useSelector } from "react-redux";
-import { View, FlatList, Text } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
 import Loading from "../components/LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
 
@@ -10,23 +18,36 @@ const FavoritesScreen = ({ navigation }) => {
   );
 
   const favorites = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
 
   const renderFavoriteItem = ({ item: campsite }) => {
     return (
-      <ListItem
-        onPress={() =>
-          navigation.navigate("Directory", {
-            screen: "CampsiteInfo",
-            params: { campsite },
-          })
-        }
-      >
-        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-        <ListItem.Content>
-          <ListItem.Title>{campsite.name}</ListItem.Title>
-          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+      <SwipeRow rightOpenValue={-100}>
+        <View style={styles.deleteView}>
+          <TouchableOpacity
+            style={styles.deleteTouchable}
+            onPress={() => dispatch(toggleFavorite(campsite.id))}
+          >
+            <Text style={styles.deleteText}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <ListItem
+            onPress={() =>
+              navigation.navigate("Directory", {
+                screen: "CampsiteInfo",
+                params: { campsite },
+              })
+            }
+          >
+            <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+            <ListItem.Content>
+              <ListItem.Title>{campsite.name}</ListItem.Title>
+              <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </View>
+      </SwipeRow>
     );
   };
 
@@ -50,5 +71,26 @@ const FavoritesScreen = ({ navigation }) => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  deleteView: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flex: 1,
+  },
+  deleteTouchable: {
+    backgroundColor: "red",
+    height: "100%",
+    justifyContent: "center",
+  },
+  deleteText: {
+    color: "white",
+    fontWeight: "700",
+    textAlign: "center",
+    fontSize: 16,
+    width: 100,
+  },
+});
 
 export default FavoritesScreen;
