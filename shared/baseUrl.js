6bfb9cfa57f1ebd5dfa2fd4348c8b9ab67c977ef
Dericknewton1979 <dericknewton1979@hotmail.com>
2023-05-19 16:17:47 -0700

    back to android

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'http://192.168.1.24:3001/';
-export const baseUrl = "https://da87-73-11-159-210.ngrok.io/";
+export const baseUrl = "http://192.168.1.24:3001/";
+// export const baseUrl = "https://da87-73-11-159-210.ngrok.io/";
